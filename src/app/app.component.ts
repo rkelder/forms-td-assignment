31d03778fb27eff202069bcf22462d180942dd07
rkelder <roderik.kelder@gmail.com>
2024-01-17 12:48:42 +0100

    Form - template driven

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'forms-td-assignment';
+  selectedSubscription = 'advanced';
+  user = {
+    email: '',
+    subscription: this.selectedSubscription,
+    password: '',
+  };
+
+  onSubmit() {
+    console.log(this.user);
+  }
 }
